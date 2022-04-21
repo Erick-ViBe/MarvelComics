b6c53cf46ae9eb3c47024c24c85218a6ba6bf2b7
Erick-ViBe <erick.vicencio@e-vanhost.net>
2022-04-21 00:02:09 -0500

    feat: add user login endpoint

--- app/resources/users.py
+++ app/resources/users.py
@@ -1,8 +1,8 @@
 # FastAPI
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 # Schemas
-from app.schemas import UserRegister, UserOut
+from app.schemas import UserRegister, UserOut, UserLogin
 
 # Database
 from app.database import users_collection
@@ -29,3 +29,14 @@ async def create_user(user: UserRegister, Authorize: AuthJWT = Depends()):
     user_dict.pop('password')
     user_dict['id_'] = str(user_id)
     return user_dict
+
+
+@router.post('/users/login/', response_model=UserOut)
+async def login(user_data: UserLogin):
+    user = users_collection.find_one({'name': user_data.name})
+    if not user:
+        raise HTTPException(status_code=400, detail="The submitted name does not exist in the system.")
+    elif not pwd_context.verify(user_data.password, user['password']):
+        raise HTTPException(status_code=400, detail="The submitted password is wrong.")
+    user['id_'] = str(user.pop('_id'))
+    return user
